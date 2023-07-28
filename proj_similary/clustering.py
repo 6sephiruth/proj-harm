df08f64e34ca32488bd5e20143c11f20c24bad10
gyumin <6sephiruth@gmail.com>
2023-07-28 15:56:11 +0900

    what the ..

--- proj_similary/clustering.py
+++ proj_similary/clustering.py
@@ -1,5 +1,112 @@
 
 from utils import *
 
+import tensorflow as tf
+import keras
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-aug_img(f'./similary_img/train')
+
+
+# from sklearn.neural_network import MLPClassifier
+
+# (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+# x_train = np.reshape(x_train, (60000, -1))
+# x_test = np.reshape(x_test, (10000, -1))
+
+# model = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+# model.fit(x_train, y_train)
+
+# print(model.score(x_test, y_test))
+
+# exit()
+
+# Uncomment this to designate a specific GPU
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+os.environ['TF_DETERMINISTIC_OPS'] = '0'
+
+# enable memory growth
+physical_devices = tf.config.list_physical_devices('GPU')
+for d in physical_devices:
+    tf.config.experimental.set_memory_growth(d, True)
+
+# 랜덤 시드 설정
+SEED = 0
+set_seed(SEED)
+
+x_raw_train = resize_img(f'./similary_img/train/')
+y_raw_train = np.array([0, 1, 2, 1, 0, 3, 1, 4, 3, 2, 0, 3, 3, 3, 2, 4, 0, 0, 2, 2, 3, 2, 3])
+
+
+x_raw_test = resize_img(f'./similary_img/test/')
+y_raw_test = np.array([4, 1, 3, 2, 0])
+
+# from sklearn.neural_network import MLPClassifier
+# x_train = np.reshape(x_raw_train, (23, -1))
+# x_test = np.reshape(x_raw_test, (5, -1))
+# model = MLPClassifier(verbose=1)
+# model.fit(x_train, y_raw_train)
+# print(model.score(x_test, y_raw_test))
+# exit()
+
+
+datagen = ImageDataGenerator(
+    featurewise_center=True,
+    featurewise_std_normalization=True,
+    rotation_range=5,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    horizontal_flip=True)
+
+datagen.fit(x_raw_train)
+x_data = []
+y_data = []
+
+e = 0
+for x_batch, y_batch in datagen.flow(x_raw_train, y_raw_train):
+    if e == 44:
+        break
+    if e == 0:
+        x_train = x_batch
+        y_train = y_batch
+    else:
+        x_train = np.concatenate([x_train, x_batch])
+        y_train = np.concatenate([y_train, y_batch])
+    e += 1
+
+e = 0
+for x_batch, y_batch in datagen.flow(x_raw_test, y_raw_test):
+    if e == 20:
+        break
+    if e == 0:
+        x_test = x_batch
+        y_test = y_batch
+    else:
+        x_test = np.concatenate([x_test, x_batch])
+        y_test = np.concatenate([y_test, y_batch])
+    e += 1
+
+from sklearn.neural_network import MLPClassifier
+print(x_train.shape)
+print(x_test.shape)
+
+x_train = np.reshape(x_train, (len(x_train), -1))
+x_test = np.reshape(x_test, (len(x_test), -1))
+
+model = MLPClassifier(verbose=1)
+model.fit(x_train, y_train)
+print(model.score(x_test, y_test))
+
+exit()
+# for i in range(len(x_data)):
+#     plt.imshow(x_data[i]*255)
+#     plt.savefig(f'./img/{i}.png')
+
+# from sklearn.neural_network import MLPClassifier
+
+# model = MLPClassifier(random_state=1, max_iter=300).fit(x_raw_train, y_raw_train)
+
+
+
+# print(model.predict(x_raw_test))
--- proj_similary/utils.py
+++ proj_similary/utils.py
@@ -1,15 +1,59 @@
 import os
+import time
+import random
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
 
-def aug_img(dir_path):
-    '''
-    폴더 내 모든 file list를 불러 온 뒤,
-    dataFrame을 하나로 묶는 과정
-    '''
+import skimage.io as io
+from skimage.transform import resize
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
+# random SEED 고정 함수
+def set_seed(num_seed):
+    random.seed(num_seed)
+    os.environ['PYTHONHASHSEED'] = str(num_seed)
+    np.random.seed(num_seed)
+    tf.random.set_seed(num_seed)
+
+def resize_img(dir_path):
+
+    target_height = 360
+    target_width = 360
+    
     file_list = []
 
     for path in os.listdir(dir_path):
         if os.path.isfile(os.path.join(dir_path, path)):
             file_list.append(dir_path + path)
+    
+    datasets = []
+        
+    for idx, each_img in enumerate(file_list):
+        
+        img = io.imread(each_img)
+        img_resize = resize(img, (target_height, target_width, 4))
+        img_resize = np.reshape(img_resize, (1, target_height, target_width, 4))
 
-    print(file_list)
+        if idx == 0:
+            datasets = img_resize
+        else:
+            datasets = np.concatenate([datasets, img_resize])            
+
+    return datasets
+
+def aug_img(x_data, y_data):
+    
+    datagen = ImageDataGenerator(
+        featurewise_center=True,
+        featurewise_std_normalization=True,
+        rotation_range=5,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        horizontal_flip=True,
+        validation_split=0.2)
+
+    datagen.fit(x_data)
+    aug_data = datagen.flow(x_data, y_data, batch_size=32, subset='training')
+    
+    return aug_data
